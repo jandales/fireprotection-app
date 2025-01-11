63df87ef94ed2a5688adce468dfb77467c29531d
jandales <jesusandales021993@gmail.com>
2025-01-11 23:25:42 +0800

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+
+class ClientController extends RegisteredUserController
+{
+     
+}
